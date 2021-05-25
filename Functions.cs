24348fa30f415880e9fa6afc3bf2b80bba2acd2d
bobby-dq <josh.quilacio@gmail.com>
2021-05-25 14:00:28 -0600

    first commit

---
+++ Functions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Functions
+    {
+        public static double Moebius(ulong n)
+        {
+            ulong p = 0;
+
+            // Check if divisible by two
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+                p++;
+
+                // Check if two is a repeat, then return 0
+                if (n % 2 == 0)
+                {
+                    return 0;
+                }
+            }
+
+            // Check for divisibility by other prime numbers
+            for (ulong i = 3; i <= n; i = i + 2 )
+            {
+                //  Check if n is divisible by i
+                if (n % i == 0)
+                {
+                    n = n / i;
+                    p++;
+
+                    // Check if i is a repeat, if so return 0
+                    if (n % i == 0)
+                    {
+                        return 0;
+                    }
+                }
+            }
+
+            // Return 1 of p is even and return -1 if p is odd
+            if (p % 2 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using ScottPlot;
+using static System.Math;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ScottPlotCookBook();
+            MoebiusPlot(10000000);
+        }
+
+        static void ScottPlotCookBook()
+        {
+            var plt = new ScottPlot.Plot(600, 400);
+
+            // sample data
+            double[] xs = DataGen.Consecutive(51);
+            double[] sin = DataGen.Sin(51);
+            double[] cos = DataGen.Cos(51);
+
+            // plot the data
+            plt.AddScatter(xs, sin);
+            plt.AddScatter(xs, cos);
+
+            // customize the axis labels
+            plt.Title("ScottPlot Quickstart");
+            plt.XLabel("Horizontal Axis");
+            plt.YLabel("Vertical Axis");
+
+            plt.SaveFig("quickstart_scatter2.png");
+        }
+
+        static void MoebiusPlot(ulong limit)
+        {
+            Plot plt = new ScottPlot.Plot(600, 400);
+
+            // generate data
+            // double[] xAxis = new double[limit];
+            double[] moebiusCoord = new double[limit];
+            double[] positiveBound = new double[limit];
+            double[] negativeBound = new double[limit];
+            double n = 0;
+            
+            for (ulong i = 0; i < limit; i++)
+            {
+                positiveBound[i] = Sqrt(i);
+                negativeBound[i] = -Sqrt(i);
+                n += (Functions.Moebius(i));
+                moebiusCoord[i] = n;
+            }
+
+            plt.AddSignal(moebiusCoord);
+            plt.AddSignal(positiveBound);
+            plt.AddSignal(negativeBound);
+            plt.SaveFig($"plot_moebius_{limit}.png");
+
+
+        }
+    }
+}
